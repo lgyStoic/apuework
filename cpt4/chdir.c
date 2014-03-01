904a75a3cfc90ea6356640c549997a6b3817f299
lgyStoic <lgy6534588@126.com>
2014-03-01 22:56:23 +0800

    add chdir

---
+++ cpt4/chdir.c
@@ -0,0 +1,33 @@
+#include <apue.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+int main(void)
+{
+    int filedeco;
+    struct stat statbuf;
+    if (stat(".", &statbuf) < 0)
+    {
+        err_sys("stat error");
+    }
+    else{
+        printf("%ld\n", (long)statbuf.st_ino);
+    }
+    if (chdir("/test") < 0)
+    {
+        err_sys("chdir error");
+    }
+    else
+    {
+        printf("change dir ok\n");
+    }
+    if (stat(".", &statbuf) < 0)
+    {
+        err_sys("stat error");
+    }
+    else{
+        printf("%ld\n", (long)statbuf.st_ino);
+    }
+    exit(0);
+}
